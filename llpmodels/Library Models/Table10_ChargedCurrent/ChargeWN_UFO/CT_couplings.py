943b444a10342d9d7fd5cffaf5421ad1f1bd7660
Joey Turnbull <93169965+JoeyTurn@users.noreply.github.com>
2023-03-27 23:42:44 -0700

    Add files via upload

---
+++ llpmodels/Library Models/Table10_ChargedCurrent/ChargeWN_UFO/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.3.31
+# Mathematica version: 11.1.1 for Linux x86 (64-bit) (April 18, 2017)
+# Date: Fri 23 Feb 2018 16:04:35
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
---
+++ llpmodels/Library Models/Table10_ChargedCurrent/ChargeWN_UFO/coupling_orders.py
@@ -0,0 +1,16 @@
+# This file was automatically created by FeynRules 2.3.31
+# Mathematica version: 11.1.1 for Linux x86 (64-bit) (April 18, 2017)
+# Date: Fri 23 Feb 2018 16:04:34
+
+
+from object_library import all_orders, CouplingOrder
+
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
